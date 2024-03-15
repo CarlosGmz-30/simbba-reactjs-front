6f54d6f70d77b24542810d7fa21e7bb4b1d366be
CarlosGmz-30 <20223tn066@utez.edu.mx>
2024-03-15 09:54:23 -0600

    Contenedores de Historical listos para aplicar graficas

--- src/pages/Historical.jsx
+++ src/pages/Historical.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import "../assets/styles/Historical.css";
+import React from "react";
 
 const Historical = () => {
-    return (
-        <div>
-            Historico
-        </div>
-    )
-}
+  return (
+    <div id="generalContainer">
+      <div id="carruselContainer">
+        <h1 className="title">Selecciona un bote de basura</h1>
+      </div>
+      <div id="graphicContainer">
+        <h1 className="title">Histórico del bote</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Historical
+export default Historical;
