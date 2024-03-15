f55c069cdb58fd0ef5961dfbddee44f40713b4b5
CarlosGmz-30 <20223tn066@utez.edu.mx>
2024-03-15 10:26:54 -0600

    Header en la pantalla del Historio

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -1,74 +1,82 @@
-import React from 'react'
-import { Avatar, Label, Button } from 'flowbite-react';
-import { Sidebar } from 'flowbite-react';
-import logo from '../assets/images/logoSIMBBA-white.png';
-import { Routes, Route } from 'react-router-dom'
-import DashboardUser from '../pages/DashboardUser';
-import DashboardBoats from '../pages/DashboardBoats';
-import Historical from '../pages/Historical';
-import '../assets/styles/AdminLayout.css'
+import React from "react";
+import { Avatar, Label, Button } from "flowbite-react";
+import { Sidebar } from "flowbite-react";
+import logo from "../assets/images/logoSIMBBA-white.png";
+import "../assets/styles/AdminLayout.css";
 import { Link, Outlet } from "react-router-dom";
 
-
 const AdminLayout = () => {
-    return (
-        <>
-            <main>
-                <Sidebar aria-label="Sidebar with multi-level dropdown example" className='sidebar bg-blue-600'>
-                    <header className='logo'>
-                        <img src={logo} alt="Logo" className="w-24 h-auto" width='100px' />
-                    </header>
-                    <div className='profile'>
-                        <Avatar
-                            src="https://avatars.dicebear.com/api/avataaars/1.svg"
-                            label="Profile"
-                            rounded
-                            size='xl'
-                        />
-                        <div className='profile-info'>
-                            <div>
-                                <Label className="info" htmlFor="email1" value="Administrador" />
-                            </div>
-                            <div>
-                                <Label className="info" htmlFor="email1" value="admin@utez.edu.mx" />
-                            </div>
-                        </div>
-                    </div>
-                    <Sidebar.Items className='side-bar'>
-                        <Sidebar.ItemGroup >
-                            <li>
-                                <Link to={"users"} className='side-bar item'>
-                                    Ver Usuarios
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={"boats"} className='side-bar item'>
-                                    Ver Botes
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to={"historical"} className='side-bar item'>
-                                    Histórico
-                                </Link>
-                            </li>
-                        </Sidebar.ItemGroup>
-                    </Sidebar.Items>
-                    <div style={{ justifyContent: 'center', alignContent: 'center', display: 'flex' }}>
-                        <Button
-                            type='submit'
-                            className='button-logout'
-                            id='btn-logout'
-                        >
-                            CERRAR SESIÓN
-                        </Button>
-                    </div>
-                </Sidebar>
-                <section style={{ width: '100%', marginTop: 60, marginBottom: 60, marginLeft: 100, marginRight: 100 }}>
-                    <Outlet />
-                </section>
-            </main>
-        </>
-    )
-}
+  return (
+    <>
+      <main>
+        <Sidebar
+          aria-label="Sidebar with multi-level dropdown example"
+          className="sidebar bg-blue-600"
+        >
+          <header className="logo">
+            <img src={logo} alt="Logo" className="w-24 h-auto" width="100px" />
+          </header>
+          <div className="profile">
+            <Avatar
+              src="https://avatars.dicebear.com/api/avataaars/1.svg"
+              label="Profile"
+              rounded
+              size="xl"
+            />
+            <div className="profile-info">
+              <div>
+                <Label
+                  className="info"
+                  htmlFor="email1"
+                  value="Administrador"
+                />
+              </div>
+              <div>
+                <Label
+                  className="info"
+                  htmlFor="email1"
+                  value="admin@utez.edu.mx"
+                />
+              </div>
+            </div>
+          </div>
+          <Sidebar.Items className="side-bar">
+            <Sidebar.ItemGroup>
+              <li>
+                <Link to={"users"} className="side-bar item">
+                  Ver Usuarios
+                </Link>
+              </li>
+              <li>
+                <Link to={"boats"} className="side-bar item">
+                  Ver Botes
+                </Link>
+              </li>
+              <li>
+                <Link to={"historical"} className="side-bar item">
+                  Histórico
+                </Link>
+              </li>
+            </Sidebar.ItemGroup>
+          </Sidebar.Items>
+          <div
+            style={{
+              justifyContent: "center",
+              alignContent: "center",
+              display: "flex",
+            }}
+          >
+            <Button type="submit" className="button-logout" id="btn-logout">
+              CERRAR SESIÓN
+            </Button>
+          </div>
+        </Sidebar>
+        <section style={{ width: "100%", margin: '0%'}}>
+          <Outlet />
+        </section>
+      </main>
+    </>
+  );
+};
 
-export default AdminLayout
+export default AdminLayout;
--- src/pages/Historical.jsx
+++ src/pages/Historical.jsx
@@ -4,12 +4,17 @@ import React from "react";
 const Historical = () => {
   return (
     <div id="generalContainer">
-      <div id="carruselContainer">
-        <h1 className="title">Selecciona un bote de basura</h1>
+      <div id="Header">
+        <h1 id="headerTitle">Histórico</h1>
       </div>
-      <div id="graphicContainer">
-        <h1 className="title">Histórico del bote</h1>
-      </div>
+      <section style={{ width: "100%", height: "100%" }}>
+        <div id="carruselContainer">
+          <h1 className="title">Selecciona un bote de basura</h1>
+        </div>
+        <div id="graphicContainer">
+          <h1 className="title">Histórico del bote</h1>
+        </div>
+      </section>
     </div>
   );
 };
