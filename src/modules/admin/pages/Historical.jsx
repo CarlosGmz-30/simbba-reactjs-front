c4e58b0b39c15b397aaa1d886776049a77a217d7
Galicia_Jimenez_Jayr_Gil <20223tn052@utez.edu.mx>
2024-03-29 13:32:43 -0600

    Trashcan carrusel need to be fixed

--- src/modules/admin/pages/Historical.jsx
+++ src/modules/admin/pages/Historical.jsx
@@ -1,7 +1,46 @@
 import "../styles/Historical.css";
-import React from "react";
+import React, { useState, useEffect } from 'react';
+
+import AxiosClient from "../../../config/http-client/axios-client";
+import TrashCard from "../components/TrashCard";
 
 const Historical = () => {
+
+  const [trashcans, setTrashcans] = useState([]);
+
+  const getAllTrashcans = async () => {
+    try {
+      const response = await AxiosClient({
+        url: "/v1/trashcan/",
+        method: "GET"
+      });
+      //console.log(response);
+
+      const DATA = response.data;
+      setTrashcans(DATA);
+
+      console.log(DATA);
+
+      DATA.forEach(trashcan => { 
+          console.log(`ID: ${trashcan.id}`);
+          console.log(`Serial Number: ${trashcan.serialNumber}`);
+          console.log(`Trashcan Name: ${trashcan.trashcanName}`);
+          console.log(`Level: ${trashcan.level}`);
+      });
+
+
+    } catch (error) {
+      console.log('Error: ', error);
+    }
+  }
+
+
+  useEffect(() => {
+    getAllTrashcans();
+  }, []);
+
+
+
   return (
     <div id="generalContainer">
       <div id="Header">
@@ -10,6 +49,13 @@ const Historical = () => {
       <section id="sectionGeneral">
         <div id="carruselContainer">
           <h1 className="title">Selecciona un bote de basura</h1>
+          {trashcans && trashcans.map((trashcan, index) => (
+            <TrashCard
+              key={index}
+              name={trashcan.trashcanName}
+              level={trashcan.level}
+            />
+          ))}
         </div>
         <div id="graphicContainer">
           <h1 className="title">Histórico del bote</h1>
